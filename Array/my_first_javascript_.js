1f0739c7b05a39a72d52eb639aae5b7d7b85f9cf
MdHasan-Ahmed <hasanahmedgraphicspear@gmail.comProfile image>
2026-01-03 15:26:47 +0600

    new fruit

--- Array/my_first_javascript_.js
+++ Array/my_first_javascript_.js
@@ -1,4 +1,7 @@
 const numberss = [71,72,73,74,75,76,77,78,79]
 console.log(numberss);
 
-const fruits = 
+let fruits = ['apple', 'banana', 'orange', 'mango', 'guava', 'nut'];
+console.log(fruits);
+
+
