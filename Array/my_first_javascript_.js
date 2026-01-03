9f57fbe42854dc884a7b6c1473e6371b6f4f0e37
MdHasan-Ahmed <hasanahmedgraphicspear@gmail.comProfile image>
2026-01-03 15:53:37 +0600

    Even and odd number logic add

--- Array/my_first_javascript_.js
+++ Array/my_first_javascript_.js
@@ -9,3 +9,24 @@ let vegetables = ['potato', 'onion', 'begun', 'radish', 'carrot', 'tomato','ladi
 console.log(vegetables);
 console.log(vegetables.length);
 
+let favouriteMovie = ['Titanic', 'Avatar', 'Outlander', 'Shawshank Redemption', 'Escap plan']
+console.log(favouriteMovie);
+console.log(favouriteMovie.length);
+let oddnumbers = [11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31]
+console.log(oddnumbers);
+
+// Odd numbers gula easily add korar sohoj code. jokhon onek besi odd numbers ber kora lagbe tokhon ei logic apply kora jete pare
+
+// bijor sonkha ber korar niyom
+
+let oddnumbers2 = [];
+for (let i =1; i<=300; i+=2){oddnumbers2.push(i)};
+console.log(oddnumbers2);
+console.log(oddnumbers2.length);
+
+// jor sonkha ber korar niyom
+
+let evennumbers = [];
+for(let i = 2; i<=300; i+=2){evennumbers.push(i)};
+console.log(evennumbers);
+
